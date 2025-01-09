07ff5ddbb31a46c17bb4554fdf1c6a210b87736e
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2025-01-09 09:37:53 +0545

    Add Form Validation and Password Hashing

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///../market/instance/market.db"
 app.config['SECRET_KEY']='f86f524bcddb98dd23d951ed'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
